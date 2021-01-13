be7e719d5a6066737a289eeb46b07eccb1efe08b
XabierUrkiola <xabier.urquiolaib@elorrieta-errekamari.com>
2021-01-13 16:16:45 +0000

    Función mostrar carta

---
+++ js/mostrarCarta.js
@@ -0,0 +1,17 @@
+$(document).ready(inicio);
+
+function inicio(){	
+	$(".enlace").click(function(){
+
+		var carta =$(this).next('div');
+		var estado = carta.css('display')
+		
+		if(estado == 'none'){
+			carta.css('display','block');
+			$(this).text("Ocultar carta");
+		}else{
+			carta.css('display','none');
+			$(this).text("Mostrar carta");
+		}
+	});
+}
