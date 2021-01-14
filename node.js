8308dcdc4fc7dee0055e57b166abc1be1c004dba
Urki <urki66@gmail.com>
2021-01-14 21:28:52 +0100

    Mover ficheros para que en node se vean los estilos

--- node.js
+++ node.js
@@ -8,21 +8,24 @@ web.listen(3000, ()=> {
 	console.log('Web corriendo en el puerto 3000')
 })
 
+
+web.use(express.static("public"))
+
 //Rutas
 web.get('/',(request,response)=> {
-	response.sendFile(path.resolve(__dirname,'../index.html'))
+	response.sendFile(path.resolve(__dirname,'index.html'))
 })
 
 web.get('/index.html',(request,response)=> {
-	response.sendFile(path.resolve(__dirname,'../index.html'))
+	response.sendFile(path.resolve(__dirname,'index.html'))
 })
 
 web.get('/restaurantes.html',(request,response)=> {
-	response.sendFile(path.resolve(__dirname,'../restaurantes.html'))
+	response.sendFile(path.resolve(__dirname,'restaurantes.html'))
 })
 
 web.get('/contacto.html',(request,response)=> {
-	response.sendFile(path.resolve(__dirname,'../contacto.html'))
+	response.sendFile(path.resolve(__dirname,'contacto.html'))
 })
 
 
