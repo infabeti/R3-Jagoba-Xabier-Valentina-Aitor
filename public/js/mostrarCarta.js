4d2b7fc7612a229d7b9ee35a946e2944eea90f69
Urki <urki66@gmail.com>
2021-01-20 16:07:46 +0100

    Cambiadas funciones a archivos js

---
+++ public/js/mostrarCarta.js
@@ -0,0 +1,14 @@
+$(document).ready();
+var oculto=true;
+	function mostrar(numero) {
+		var nombre = 'carta'+numero;
+		
+		
+		if(oculto==true){
+			document.getElementById(nombre).style.display = 'block';
+			oculto=false;
+		}else{
+			document.getElementById(nombre).style.display = 'none';
+			oculto=true;
+		}
+	}
---
+++ public/js/mostrarOpiniones.js
@@ -0,0 +1,15 @@
+$(document).ready();
+var oculto=true;
+
+function mostrar(numero) {
+	var nombre = 'opinion'+numero;
+	
+	
+	if(oculto==true){
+		document.getElementById(nombre).style.display = 'block';
+		oculto=false;
+	}else{
+		document.getElementById(nombre).style.display = 'none';
+		oculto=true;
+	}
+}
