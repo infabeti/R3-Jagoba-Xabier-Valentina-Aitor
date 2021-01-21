af79f3836de30962781f60fd44afe905a974ddde
JagobaRuiz <jagoba.ruizab@elorrieta-errekamari.com>
2021-01-21 17:16:05 +0100

    Finalizados los test

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,16 @@
-const calcularPedido = require('../public/js/calcularPedido.js');
+const calcularPedido = require('../test/calcular.js');
+const calcularAforo = require('../test/aforo.js');
 
 describe("Mis tests de calculo del pedido", function() {
   it("Debería multiplicar", function() {
-    expect(calcularPedido.calcular(2,2)).toBe(4.84);
+    expect(calcularPedido.calcular(1.20)).toBe('7.26');
   });
 
 });
 
+describe("Mis tests de calculo de aforo", function() {
+  it("aforo=tamanio/4", function() {
+    expect(calcularAforo.aforo()).toBe(2);
+  });
+
+});
