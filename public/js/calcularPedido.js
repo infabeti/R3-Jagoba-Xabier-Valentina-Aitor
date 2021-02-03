27fe560441f974e16b659535fbc5fe02c919b490
in2daw <in2daw@elorrieta.int>
2021-02-03 19:14:17 +0100

    conexion con API java

--- public/js/calcularPedido.js
+++ public/js/calcularPedido.js
@@ -11,3 +11,29 @@ function calcular(precio) {
 	}
 
 } 
+
+
+function llamadoApi(){
+    
+    
+    fetch('http://localhost:8080/Productoss/',{
+        
+    }).then((response) => response.json())
+    .then(data =>{
+
+
+		for(i=0; i<13; i++){
+       
+
+		 var datos=Object.values(data.productosList[i])+"<br>";
+		
+
+		 var nuevosdatos = datos.replace(/,/g, ' ');
+		 
+
+		 $("#datosCarta ul").append('<li>'+nuevosdatos.slice(2)+'</li>');
+			   
+		}
+        
+    });
+}
